83437e31ac46e0129ce13b1b3490d5143fc33aee
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-13 15:33:18 +0530

    Started solving excercise 2-65

---
+++ ch2/2-65.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Return 1 when x contains an odd number of 1s; 0 otherwise.
+   Assume w=32 */
+int odd_ones(unsigned x)
+{
+  //At most 12 arthmatic, logical and bitwise operations
+  
+  
+}
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
