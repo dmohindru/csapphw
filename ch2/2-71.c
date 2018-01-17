5fa0d94887b2d9f01ee06384589cc1b4b60c015e
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-17 12:47:10 +0530

    Completed excercise 2-71

--- ch2/2-71.c
+++ ch2/2-71.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-/
 typedef unsigned packed_t;
 
 /* Answer A: below code does not sign extent the extracted byte
@@ -12,10 +11,22 @@ int xbyte(packed_t word, int bytenum)
 /* Answer B: correct code with sign extention */
 int xbyte(packed_t word, int bytenum)
 {
-  return (word >> (bytenum << 3)) && 0xFF;
+  /* My Solution
+  int w = sizeof(int) << 3;
+  word <<= w - (bytenum  << 3) - 8;
+  return (int) word >> (w - 8);
+  */
+  /* other solution as seem quite similar */
+  int w = sizeof(unsigned);
+  int left_shift = (w - 1 - bytenum) << 3;
+  int right_shift = (w - 1) << 3;
+  return (int) word << left_shift >> right_shift;
 }
 int main(int argc, char *argv[])
 {
-  
+  printf("%X\n", xbyte(0x8F77F011, 3));
+  printf("%X\n", xbyte(0x8F77F011, 2));
+  printf("%X\n", xbyte(0x8F77F011, 1));
+  printf("%X\n", xbyte(0x8F77F011, 0));
   return 0;
 }
