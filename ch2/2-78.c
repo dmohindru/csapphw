ddfba55186e4aebf4effb09711d0719b40eed741
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-19 09:10:18 +0530

    completed excercise 2-80

--- ch2/2-78.c
+++ ch2/2-78.c
@@ -5,11 +5,12 @@ int divide_power2(int x, int k)
 {
   int is_neg = x & INT_MIN;
   (is_neg && (x = x + (1 << k) -1)); 
+  //(is_neg && (x = x + (1 << k)));
   return x >> k;
 }
 int main(int argc, char *argv[])
 {
   printf("%d\n", divide_power2(100, 2));
-  printf("%d\n", divide_power2(-100, 2));
+  printf("%d\n", divide_power2(-16, 3));
   return 0;
 }
--- ch2/2-80.c
+++ ch2/2-80.c
@@ -3,10 +3,22 @@
 
 int mul3div4(int x)
 {
-  int is_neg = x & INT_MIN;
+  /*int is_neg = x & INT_MIN;
   (is_neg && (x = x + (1 << 2) - 1));
   x >>= 2;
-  return (x << 1) + x;
+  return (x << 1) + x;*/
+  int is_neg = x & INT_MIN;
+  int m = x & ~0x3;
+  int l = x & 0x3;
+  //calculation for m part
+  int md4 = m >> 2;
+  int md4m3 = (md4 << 1) + md4;
+  //calcuation for l part
+  int lm3 = (l << 1) + l;
+  int bais = (1 << 2) - 1;
+  (is_neg && (lm3 += bais));
+  int lm3d4 =  lm3 >> 2;
+  return (md4m3 + lm3d4);
 }
 int main(int argc, char *argv[])
 {
