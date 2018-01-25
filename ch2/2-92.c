93715b68ce1205f7783b0871686b16b6c6835124
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-25 18:48:03 +0530

    Completed excercise 2-92

--- ch2/2-92.c
+++ ch2/2-92.c
@@ -9,6 +9,7 @@ float_bits float_negetive(float_bits f)
 {
   unsigned exp = f >> 23 & 0xFF;
   unsigned frac = f & 0x7FFFFF;
+  unsigned u;
   if (exp == 0xFF && frac != 0) //its a NaN
     return f;
   else
@@ -16,32 +17,5 @@ float_bits float_negetive(float_bits f)
 }
 int main(int argc, char *argv[])
 {
-  int i = 0;
-  float_bits f = 0;
-  //printf("%X\n", UINT_MAX);
-  //printf("%X\n", (unsigned)NAN);
-  //printf("%X\n", (unsigned)INFINITY);
-  //printf("%X\n", (unsigned)-INFINITY);
-  //printf("%f\n", (float)0x00000000);
-  //printf("%f\n", (float)0x8000000000000000);
-  //printf("%f\n", (float)0x7F800001);
-  //printf("%f\n", (float)0x7FFFFFFF);
-  for (i = 0; i <= UINT_MAX; i++)
-  {
-    if ((i & 0x7FFFFFFF) >= 0x7F800001 || (i & 0x7FFFFFFF)  <= 0x7FFFFFFF) //its NAN
-    {
-    }
-    else
-    {
-      if(-(float) i != float_negetive(i))
-      {
-        printf("test failed for value: %d\n", i);
-        break;
-      }
-    }
-  }
-  //UINT_MAX
-  //NAN;
-  //INFINITY;
   return 0;
 }
