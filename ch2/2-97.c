c5b82df3fee93b2adaa754e8f473cb19057e0ec2
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-27 16:05:18 +0530

    Almost completed excercise 2-97

--- ch2/2-97.c
+++ ch2/2-97.c
@@ -2,7 +2,10 @@
 #include <limits.h>
 
 typedef unsigned float_bits;
-
+/*
+ * TODO: check for INT_MAX case
+ * study round to zero , round to even case
+ */
 /* Calculate bit length */
 int bit_length(int i)
 {
@@ -36,30 +39,56 @@ float_bits float_i2f(int i)
  * 4. frac = Mantisa_mask & number
  * 5. assemble bits sign << 31 | e << 23 | frac
  */
- unsigned sign, e, E, frac, bias = 127;
+ unsigned sign, e, E, frac, fmask, bias = 127;
+ int num, len;
  sign = i >> 31;
  if (i == INT_MIN)
  {
    E = 31;
+   e = E + bias; 
    frac = 0;
  }
  else
  {
-   
-   
+   //num = sign ? -i : i;
+   len = bit_length(i);
+   fmask = frac_mask(len);
+   E = len - 1;
+   e = bias + E;
+   num = sign ? -i : i;
+   frac = num & fmask;
+   if (E > 23)
+   {
+     //need to set condition for round to zero rule
+     frac >>= (E - 23);
+   }
+   else
+   {
+     frac <<= (23 - E);
+   }
  }
- 
+ return sign << 31 | e << 23 | frac;
 }
 int main(int argc, char *argv[])
 {
   int len;
   unsigned frac_bit_mask;
-  len = bit_length(INT_MAX);
-  frac_bit_mask = frac_mask(len);
-  printf("%d\n", len);
-  printf("%X\n", frac_bit_mask);
+  //len = bit_length(INT_MAX);
+  //frac_bit_mask = frac_mask(len);
+  //printf("%d\n", len);
+  //printf("%X\n", frac_bit_mask);
   //printf("%d\n", bit_length(-125698));
   //printf("%d\n", bit_length(-9999));
   //printf("%d\n", bit_length(INT_MIN)); 0xFFFF0
+  printf("%X\n", float_i2f(1000));
+  printf("%X\n", float_i2f(-1000));
+  printf("%X\n", float_i2f(132678));
+  printf("%X\n", float_i2f(-132678));
+  printf("%X\n", float_i2f(1));
+  printf("%X\n", float_i2f(-1));
+  printf("%X\n", float_i2f(9876543));
+  printf("%X\n", float_i2f(-9876543));
+  printf("%X\n", float_i2f(INT_MIN));
+  printf("%X\n", float_i2f(INT_MAX));
   return 0;
 }
