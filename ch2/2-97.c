cda212c9b07b15b08553435a06c93d5dea74f6d9
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-29 09:56:46 +0530

    Finally completed excercise 2-97 phwee.....

--- ch2/2-97.c
+++ ch2/2-97.c
@@ -7,7 +7,7 @@ typedef unsigned float_bits;
  * study round to zero , round to even case
  */
 /* Calculate bit length */
-int bit_length(int i)
+/*int bit_length(int i)
 {
   int length = 0;
   if (i == INT_MIN)
@@ -21,15 +21,15 @@ int bit_length(int i)
     i = (unsigned)i >> 1;
   }
   return length;
-}
+}*/
 
 /* generate frac bits mask */
-unsigned frac_mask(int length)
+/*unsigned frac_mask(int length)
 {
   return (unsigned)-1 >> (32-length+1);
 }
 /* Computer (float) i */
-float_bits float_i2f(int i)
+/*float_bits float_i2f(int i)
 {
 /* 1. Get sign bit
  * 2. Calculate leftmost position E of 1 in a positive number
@@ -39,7 +39,7 @@ float_bits float_i2f(int i)
  * 4. frac = Mantisa_mask & number
  * 5. assemble bits sign << 31 | e << 23 | frac
  */
- unsigned sign, e, E, frac, fmask, bias = 127;
+ /*unsigned sign, e, E, frac, fmask, bias = 127;
  int num, len;
  sign = i >> 31;
  if (i == INT_MIN)
@@ -68,6 +68,26 @@ float_bits float_i2f(int i)
    }
  }
  return sign << 31 | e << 23 | frac;
+}*/
+float_bits float_i2f(int x) {
+    int e = 158;
+    int mask = 1<<31;
+    int sign = x&mask;
+    int frac;
+    if (x == mask)
+        return mask | (158<<23);
+    if (!x)
+        return 0;
+    if (sign)
+        x = ~x + 1;
+    while (!(x&mask)) {
+        x = x<<1;
+        e = e -1;
+    }
+    frac = (x&(~mask)) >> 8;
+    if (x&0x80 && ((x&0x7F) > 0 || frac&1)) //round off condition
+        frac = frac + 1;
+    return sign + (e<<23) + frac;
 }
 int main(int argc, char *argv[])
 {
