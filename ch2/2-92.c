2eb3389fd0d05c944bf7dab7f6010c6b72caaeeb
Dhruv Mohindru <dmohindru@gmail.com>
2018-01-30 19:09:30 +0530

    Started testing excercise 2-92

--- ch2/2-92.c
+++ ch2/2-92.c
@@ -3,7 +3,14 @@
 #include <limits.h>
 
 typedef unsigned float_bits;
+/* Helper function*/
+unsigned f2u(float f) {
+  return *(unsigned*) &f;
+}
 
+float u2f(unsigned u) {
+  return *(float*) &u;
+}
 /* Compute -f. If f in NaN, then return f. */
 float_bits float_negetive(float_bits f)
 {
@@ -17,5 +24,13 @@ float_bits float_negetive(float_bits f)
 }
 int main(int argc, char *argv[])
 {
+  unsigned f = 0x4640E400;
+  unsigned f_neg = float_negetive(f);
+  //float f_num = u2f(f);
+  //float test_num = -12345.00;
+  float test_num = u2f(0xC640E400);
+  unsigned test_f_neg = f2u(test_num);
+  printf("%d\n", f_neg == test_f_neg);
+  
   return 0;
 }
