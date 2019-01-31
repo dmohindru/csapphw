5f9e2dae88d6a92313fca4ccccf7185d5967ce54
Dhruv Mohindru <dmohindru@gmail.com>
2019-01-31 09:44:12 +0530

    Playing around with ex 11-2, 11-3

---
+++ ch11/11-3.c
@@ -0,0 +1,20 @@
+#include "csapp.h"
+
+int main(int argc, char *argv[])
+{
+    struct in_addr inaddr; /* Address in network byte order */
+    int rc;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <network byte order>\n", argv[0]);
+        exit(0);
+    }
+    rc = inet_pton(AF_INET, argv[1], &inaddr);
+    if (rc ==0)
+        app_error("inet_pton error: invalid network byte order");
+    else if (rc < 0)
+        unix_error("inet_pton error");
+
+    printf("0x%x\n", ntohs(inaddr.s_addr));
+    exit(0);
+}
