a60f7b79a5acc7323b3f68604ebf149b8ff4686e
Dhruv Mohindru <dmohindru@gmail.com>
2019-03-06 12:05:08 +0530

    Solved hw ex 12-33

--- ch12/tfgets-thread.c
+++ ch12/tfgets-thread.c
@@ -9,15 +9,41 @@ struct pack
 void *read_thread(void *vargp)
 {
     struct pack *args = (struct pack*)(vargp);
+    fgets(args->s, args->size, args->stream);
+    time_out = 0;
+    return NULL;
 
 }
-void *sleep_thread(void *vargp);
+void *sleep_thread(void *vargp)
 {
-
+    sleep(5);
+    time_out = 1;
+    return NULL;
 }
 
 char *tfgets(char *s, int size, FILE *stream)
 {
-    fgets(s, size, stream);
-    return s;
+    pthread_t readt, sleept;
+    struct pack arg_pack;
+    arg_pack.s = s;
+    arg_pack.size = size;
+    arg_pack.stream = stream;
+    /* Create read threads */
+    Pthread_create(&readt, NULL, read_thread,(void *)&arg_pack);
+    /* Create sleep thread */
+    Pthread_create(&sleept, NULL, sleep_thread, NULL);
+    
+    /* Wait for either thread to finish */
+    while (time_out == -1);
+
+    if (time_out == 0)  /* Got the input in time */
+    {
+        Pthread_cancel(sleept);
+        return arg_pack.s;
+    }
+    else /* Time out happened */
+    {
+        Pthread_cancel(readt);
+        return NULL;
+    }
 }
